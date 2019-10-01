e90614ae9071d13a436a661d69d97e98a175e6da
gchan93 <gyungchaehan@woodstock.ac.in>
2019-10-01 09:40:03 +0530

    inital push

--- beta.py
+++ beta.py
@@ -14,16 +14,16 @@ print("Welcome!")
 print("Sorted: The simple text-base task manager.")
 
 if stage == 1:
-    mode = input("Press [v] tp view tasks, or [a] to add tasks.")
+    mode = input("Press [v] tp view tasks, or [a] to add tasks: ")
     if mode == "v":
         stage = 2
     elif mode == "a":
         stage = 3
 
 if stage == 2:
-    print("Your current tasks are:")
+    print("Your current tasks are: ")
     if taskn == 0:
-        print("You don't have any tasks. Please add them first.")
+        print("You don't have any tasks. Please add them first: ")
         stage = 3
     elif taskn == 1:
         print(task1,due1)
@@ -36,17 +36,17 @@ if stage == 2:
 
 if stage == 3:
     if taskn == 0:
-        task1 = input("What is your task?")
+        task1 = input("What is your task? ")
         print("Task added!")
         taskn = 1
         stage = 1
     elif taskn == 2:
-        task2 = input("What is your task?")
+        task2 = input("What is your task? ")
     elif taskn == 3:
-        task3 = input("What is your task?")
+        task3 = input("What is your task? ")
     else:
         print("Max number of tasks reached!")
-        print("Would you like to view them?")
+        print("Would you like to view them? ")
         view = input("[y/n]")
         if view == "y":
             stage = 2
